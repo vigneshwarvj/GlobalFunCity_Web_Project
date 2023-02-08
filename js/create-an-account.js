c0672a919909a6d040903d893909d4b30091e54f
vigneshwarvj <vigneshwar.vijayakumar@fwsa.freshworks.com>
2023-02-08 15:29:07 +0530

    Localstorage

---
+++ js/create-an-account.js
@@ -0,0 +1,36 @@
+const create = e => {
+    let first_name = document.getElementById("first_name") .value
+    let middle_name = document.getElementById("middle_name") .value
+    let last_name = document.getElementById("last_name") .value
+    let email = document.getElementById("email") .value
+    let password = document.getElementById("password") .value
+    let re_enter_password = document.getElementById("re_enter_password") .value
+    let date = document.getElementById("date") .value
+    let right = document .getElementById("right") .value
+    
+    // Parshing the JSON
+    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
+    let exist = user_list.length &&
+        JSON.parse(localStorage.getItem('user_list')).some(data =>
+        data.email.toLowerCase() == email.toLowerCase() 
+         );
+    
+    if(!exist){
+        user_list.push({first_name, middle_name, last_name, email, password, re_enter_password, date, right});
+        localStorage.setItem('user_list' , JSON.stringify(user_list));
+        console.log(localStorage.getItem('user_list'));
+        document.querySelector('form').reset();
+        document.getElementById('email').focus();
+        
+        alert('Account Created Successfully..')
+        location.href="../signup for booking/signup.html";
+    }
+    
+    else{
+        alert('Oops! Something went wrong. Your email or password is invalid');
+        document.querySelector('form').reset();
+    }
+    e.preventDefault();
+    
+    }
+    
