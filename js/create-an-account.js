21fdb698db1a407094cebafdfbaf83902b54f91a
vigneshwarvj <vigneshwar.vijayakumar@fwsa.freshworks.com>
2023-03-11 22:22:37 +0530

    Updated

--- js/create-an-account.js
+++ js/create-an-account.js
@@ -6,28 +6,47 @@ const create = e => {
     let password = document.getElementById("password") .value
     let re_enter_password = document.getElementById("re_enter_password") .value
     let date = document.getElementById("date") .value
-    let right = document .getElementById("right") .value
+    let user_uniqueId = uuidv4()
+    let mobile_number= "";
+    let home_mobile_number="";
     
     // Parshing the JSON
     let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
     let exist = user_list.length &&
         JSON.parse(localStorage.getItem('user_list')).some(data =>
-        data.email.toLowerCase() == email.toLowerCase() 
-         );
+        data.email.toLowerCase() == email.toLowerCase() );
     
     if(!exist){
-        user_list.push({first_name, middle_name, last_name, email, password, re_enter_password, date, right});
-        localStorage.setItem('user_list' , JSON.stringify(user_list));
-        console.log(localStorage.getItem('user_list'));
-        document.querySelector('form').reset();
-        document.getElementById('email').focus();
-        
-        alert('Account Created Successfully..')
-        location.href="../signup for booking/signup.html";
+        if(password == re_enter_password) {
+            user_list.push({"first_name":first_name,
+            "middle_name":middle_name, 
+            "last_name":last_name,
+            "email": email,
+            "password": password,
+            "re_enter_password": re_enter_password,
+            "date": date,
+            "mobile_number":mobile_number,
+            "home_mobile_number":home_mobile_number,
+            user_uniqueId
+            });
+            
+            localStorage.setItem('user_list' , JSON.stringify(user_list));
+            
+            console.log(localStorage.getItem('user_list'));
+            document.querySelector('form').reset();
+            document.getElementById('email').focus();
+            
+            alert('Account Created Successfully..')
+            location.href="../signup for booking/signup.html";
+        }
+        else{
+            alert("Invalid Password")
+        }
+       
     }
     
     else{
-        alert('Oops! Something went wrong. Your email or password is invalid');
+        alert('Oops! Something went wrong. Your email is already exist');
         document.querySelector('form').reset();
     }
     e.preventDefault();
